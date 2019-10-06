67d89af55c777cb2c8d89cc545166e54009b8cfd
Luca Deri <deri@ntop.org>
2019-10-06 18:14:12 +0200

    Execution fixes

--- utils/docker_show_veth.sh
+++ utils/docker_show_veth.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+#
+# Prints the veth used by running docker containers
+#
+
 if [ "$EUID" -ne 0 ]
 then echo "Please run as root"
      exit
 fi
 
-for container in $(docker ps --format '{{.Names}}'); do
-    iflink=docker exec -it $container bash -c 'cat /sys/class/net/eth*/iflink'
-    for net in $iflink;do
-	net=echo $net|tr -d '\r'
-	veth=grep -l $net /sys/class/net/veth*/ifindex
-	veth=echo $veth|sed -e 's;^.*net/\(.*\)/ifindex$;\1;'
-	echo $container:$veth
+echo "veth        containerId"
+echo "-----------------------"
+
+for container in $(docker ps --format '{{.Names}}'); do    
+    iflink=$(docker exec -it $container bash -c 'cat /sys/class/net/eth*/iflink')
+    for net in $iflink; do
+	net=$(echo $net|tr -d '\r')
+	veth=$(grep -l $net /sys/class/net/veth*/ifindex)
+	veth=$(echo $veth|sed -e 's;^.*net/\(.*\)/ifindex$;\1;')
+	echo $veth   $container
     done
 done
